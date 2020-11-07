880fc0357a4864b256dd05012cbc717b04297a53
Mohammed Shoaib <shoaibbilal101@gmail.com>
2020-11-07 14:32:17 +0500

    123

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import AppRouter from './config/router';
+
+function App() {
+  return (
+
+    <div>
+
+      <AppRouter/>
+    </div>
+    // <BrowserRouter>
+    // <Header/>
+    // <Route path="/" component = {HomePage}/>
+   
+    // <Footer/>
+    // </BrowserRouter>
+    
+  );
+}
+
+export default App;
